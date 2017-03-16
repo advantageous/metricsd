0dee0dcc991472dcc897f5c1b2dfcaad5dbb5f06
Scott Fauerbach <scottfauerbach@gmail.com>
2017-03-16 17:58:57 -0400

    more nodetools(2)

--- gatherer/nodetool/nodetool_ statuses.go
+++ gatherer/nodetool/nodetool_ statuses.go
@@ -11,6 +11,7 @@ func Statuses(nodetoolCommand string) ([]c.Metric, error) {
 	//statusgossip                 Status of gossip
 	//statushandoff                Status of storing future hints on the current node
 	//statusthrift                 Status of thrift server
+	//version I DID THIS HERE INSTEAD OF HAVING ANOTHER WHOLE TOOL
 
 	var metrics = []c.Metric{}
 	for _,ntfun := range []string{"statusbackup", "statusbinary", "statusgossip", "statushandoff", "statusthrift"} {
@@ -19,6 +20,15 @@ func Statuses(nodetoolCommand string) ([]c.Metric, error) {
 			return nil, err
 		}
 		name := "ntStatus" + c.UpFirst(ntfun[6:])
+		metrics = append(metrics, *c.NewMetricString(strings.TrimSuffix(output, c.NEWLINE), name, c.PROVIDER_NODETOOL))
+	}
+
+	for _,ntfun := range []string{"version"} {
+		output, err := c.ExecCommand(nodetoolCommand, ntfun)
+		if err != nil {
+			return nil, err
+		}
+		name := "ntStatus" + c.UpFirst(ntfun)
 		metrics = append(metrics, *c.NewMetricString(strings.TrimSuffix(output, c.NEWLINE), name, c.PROVIDER_NODETOOL))
 	}
 
